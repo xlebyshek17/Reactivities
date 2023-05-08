5da75707a0ffe5917d336ba42bfe129527cd8689
Kristina <krystina.mironenko@gmail.com>
2023-05-08 18:41:24 +0200

    created reusable form using Formik, add validation

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,6 +3,7 @@ import 'react-toastify/dist/ReactToastify.min.css';
 import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
 import 'react-calendar/dist/Calendar.css';
+import 'react-datepicker/dist/react-datepicker.css'
 import reportWebVitals from './reportWebVitals';
 
 import { StoreContext, store } from './app/stores/store';
